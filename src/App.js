fbe880ee22f84643b4c389d8a8ba8716bf6d33d2
Haley Hicks <haleyhicks90@yahoo.com>
2021-08-05 17:48:15 -0400

    outline for front end mostly set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
+//Dependencies
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <h1>hello world</h1>
-  );
+const App = () => {
+
+    ////////////////////////////////////////////////////////////////////////////
+    //state hooks
+    ////////////////////////////////////////////////////////////////////////////
+    const [newName, setNewName] = useState('')
+    const [newCategory, setNewCategory] = useState('')
+    const [newImage, setNewImage] = useState('')
+    const [recipes, setRecipes] = useState([])
+
+    ////////////////////////////////////////////////////////////////////////////
+    //invoking useEffect
+    ////////////////////////////////////////////////////////////////////////////
+    useEffect(() => {
+        axios
+            .get('http://localhost:3000/recipes')
+            .then((response) => {
+                // console.log(response);
+                setRecipes(response.data)
+            })
+    }, [])
+
+    ////////////////////////////////////////////////////////////////////////////
+    //event handlers
+    ////////////////////////////////////////////////////////////////////////////
+    const handleNewName = (event) => {
+        // console.log(event.target.value);
+        setNewName(event.target.value)
+    }
+
+    const handleNewCategory = (event) => {
+        // console.log(event.target.value);
+        setNewCategory(event.target.value)
+    }
+
+    const handleNewImage = (event) => {
+        // console.log(event.target.value);
+        setNewImage(event.target.value)
+    }
+
+    const handleNewRecipeFormSubmit = (event) => {
+        event.preventDefault()
+        // console.log(newName);
+        // console.log(newCategory);
+        // console.log(newImage);
+        axios.post('http://localhost:3000/recipes',
+            {
+                name: newName,
+                category: newCategory,
+                image: newImage
+            }
+        )
+    }
+
+    return (
+        <main>
+            <h1>Recipe List</h1>
+            <section>
+                <form onSubmit={handleNewRecipeFormSubmit}>
+                    Name: <input type='text' onChange={handleNewName} /> <br/>
+                    Category: <input type='text' onChange={handleNewCategory} /> <br/>
+                    Image: <input typr='text' onChange={handleNewImage} /> <br/>
+                    <input type='submit' value="Create Recipe" />
+                </form>
+            </section>
+        </main>
+    )
 }
-
-export default App;
